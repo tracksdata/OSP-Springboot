2f470a70f4914654868c06e9b9bfc1249da3f930
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2020-02-21 11:30:01 +0530

    Spring Final Session

--- Spring/Spring-Rest-Demo/src/main/java/com/cts/product/entity/Product.java
+++ Spring/Spring-Rest-Demo/src/main/java/com/cts/product/entity/Product.java
@@ -1,10 +1,49 @@
 package com.cts.product.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Product {
 
 	private int prodId;
 	private String prodName;
 	private double price;
+	
+	private LocalDateTime date;
+
+	private List<String> listProds;
+
+	public List<String> getListProds() {
+		return listProds;
+	}
+
+	
+	
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+
+
+	public void setListProds(List<String> listProds) {
+		this.listProds = listProds;
+	}
+
+	private Store store;
+
+	public Store getStore() {
+		return store;
+	}
+
+	public void setStore(Store store) {
+		this.store = store;
+	}
 
 	public int getProdId() {
 		return prodId;
