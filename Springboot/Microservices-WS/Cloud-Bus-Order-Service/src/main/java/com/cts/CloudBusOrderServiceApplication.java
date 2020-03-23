1f81edeca4f6d8b6ee96b6a698bd620c5ec2f95e
ctsjava <ctsgit@gmail.com>
2020-03-23 11:25:26 +0530

    Spring Cloud Bus

---
+++ Springboot/Microservices-WS/Cloud-Bus-Order-Service/src/main/java/com/cts/CloudBusOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBusOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBusOrderServiceApplication.class, args);
+	}
+
+}
